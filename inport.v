95d8e47daadfaf50fb3f263ae2dc4eca0ff36933
VinceSong91 <129912482+VinceSong91@users.noreply.github.com>
2025-03-24 11:54:13 -0400

    Update inport.v

--- inport.v
+++ inport.v
@@ -1,6 +1,5 @@
 module inport(
     input wire clk, clear,
-    // If your design expects to store external data, you can add an enable here too
     input wire [31:0] d,
     output reg [31:0] q
 );
@@ -9,7 +8,7 @@ module inport(
     if (clear)
       q <= 32'b0;
     else
-      q <= d;       // or latch if you have an enable signal
+      q <= d;      
   end
 
 endmodule
